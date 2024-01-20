e83ed6630e99f7a6abb18b6f08bf3e681b8ae5cb
lindsey-chang <lindsey_chang@qq.com>
2024-01-20 23:03:33 +0800

    v1.2
    解决了非本地模型的问题

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ from langchain.chains import RetrievalQA
 
 import gradio as gr
 
+def init():
+    model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b'
+                                  , cache_dir='./', revision='v1.0.3')
+    os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+    # 下载模型
+    os.system('huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir sentence-transformer')
+    
 
 def load_chain():
     # 加载问答链
     # 定义 Embeddings
-    embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/sentence-transformer")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformer")
 
     # 向量数据库持久化路径
     persist_directory = 'data_base/vector_db/chroma'
@@ -23,7 +30,7 @@ def load_chain():
     )
 
     # 加载自定义 LLM
-    llm = InternLM_LLM(model_path = "/root/data/model/Shanghai_AI_Laboratory/internlm-chat-7b")
+    llm = InternLM_LLM(model_path = "Shanghai_AI_Laboratory/internlm-chat-7b")
 
     # 定义一个 Prompt Template
     template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
